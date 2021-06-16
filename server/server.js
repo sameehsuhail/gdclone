4a18783e71aa967c98b1850f0d59829c00e505a7
Sameeh <sameehsuhail5@gmail.com>
2021-06-16 10:18:34 -0700

    server update

--- server/server.js
+++ server/server.js
@@ -1,42 +1,42 @@
 const mongoose = require("mongoose")
 const Document = require("./Document")
 
-mongoose.connect('mongodb://localhost/gdocclone', {
+mongoose.connect("mongodb://localhost/google-docs-clone", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
-  useCreateIndex: true
-});
+  useCreateIndex: true,
+})
 
-const io = require('socket.io')(3001, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods:["GET", "POST"],
-    },
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 })
 
 const defaultValue = ""
 
 io.on("connection", socket => {
-    socket.on('get-document', async documentId => {
-        const document = await findOrCreateDocument(documentId)
-        socket.join(documentId)
-        socket.emit('load-document', document.data)
-        socket.on('send-changes', delta => {
-            socket.broadcast.to(documentId).emit("receive-changes", delta)
-        })
-        socket.on("save-document", async data => {
+  socket.on("get-document", async documentId => {
+    const document = await findOrCreateDocument(documentId)
+    socket.join(documentId)
+    socket.emit("load-document", document.data)
 
-            await Document.findByIdAndUpdate(documentId, {data})
-        })
+    socket.on("send-changes", delta => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta)
     })
-    console.log("connected")
+
+    socket.on("save-document", async data => {
+      await Document.findByIdAndUpdate(documentId, { data })
+    })
+  })
 })
 
 async function findOrCreateDocument(id) {
-    if(id == null) return
+  if (id == null) return
 
-    const document = await Document.findById(id)
-    if(document) return document
-    return await Document.create({ _id: id, data: defaultValue})
+  const document = await Document.findById(id)
+  if (document) return document
+  return await Document.create({ _id: id, data: defaultValue })
 }
